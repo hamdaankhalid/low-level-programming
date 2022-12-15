0f0e320677e7ca27d7a1c855bce0f77058b99eb5
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-14 23:21:02 -0800

    made changes to free

--- low_level_programming_x86_64_and_c/linked-list/main.c
+++ low_level_programming_x86_64_and_c/linked-list/main.c
@@ -12,24 +12,17 @@ struct LinkedList
 struct LinkedList* build_linked_list(size_t* out_len) {
   size_t len;
   scanf("%zu", &len);
-  
   size_t linkedlistsize = sizeof(struct LinkedList);
-
   struct LinkedList* dummy_head = malloc(linkedlistsize);
   dummy_head->value = 0;
   dummy_head->next = NULL;
   dummy_head->prev = NULL;
-
   struct LinkedList* prev = NULL;
-
-  // build from tail to head and return the tail
   for (size_t i  = 0 ; i < len; i++) {
     int val;
     scanf("%d", &val);
-    
     struct LinkedList* node = malloc(linkedlistsize);
     node->value = val;
-
     if (prev != NULL) {
       node->next = prev;
     } else {
@@ -37,18 +30,14 @@ struct LinkedList* build_linked_list(size_t* out_len) {
         prev->value = val;
         prev->next = NULL;
     }
-    
     prev->prev = node;
     prev = node;
   }
-
   dummy_head->next = prev;
   prev->prev = dummy_head;
-
   struct LinkedList* curr = dummy_head->next;
   curr->prev = NULL;
   free(dummy_head);
- 
   return curr;
 }
 
@@ -56,19 +45,19 @@ struct LinkedList* build_linked_list(size_t* out_len) {
 int main(void) {
   size_t len;
   struct LinkedList* head = build_linked_list(&len);
-  struct LinkedList* curr = head;
   
+  struct LinkedList* curr = head;
   while (curr != NULL)
   {
     printf("%d ", curr->value);
     struct LinkedList* temp = curr;
     curr = curr->next;
-    
+  
     free(temp);
   }
+
   puts("\n");
   
-  free(curr);
 
   return 0;
 }
