7a66d337f7436ab82ed101dde32e25dd7c753a82
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-15 10:51:25 -0800

    linked list assignment completed

--- low_level_programming_x86_64_and_c/linked-list/main.c
+++ low_level_programming_x86_64_and_c/linked-list/main.c
@@ -11,6 +11,7 @@ struct LinkedList
 
 struct LinkedList* build_linked_list(size_t* out_len) {
   size_t len;
+  puts("Enter the length of your linkedlist: ");
   scanf("%zu", &len);
   size_t linkedlistsize = sizeof(struct LinkedList);
   struct LinkedList* dummy_head = malloc(linkedlistsize);
@@ -20,6 +21,7 @@ struct LinkedList* build_linked_list(size_t* out_len) {
   struct LinkedList* prev = NULL;
   for (size_t i  = 0 ; i < len; i++) {
     int val;
+    printf("Enter value for node at index %d: ", len-i-1);
     scanf("%d", &val);
     struct LinkedList* node = malloc(linkedlistsize);
     node->value = val;
@@ -41,11 +43,46 @@ struct LinkedList* build_linked_list(size_t* out_len) {
   return curr;
 }
 
+const int summation(struct LinkedList* head) {
+  int sum = 0;
+  struct LinkedList* curr = head;
+  while (curr != NULL)
+  {
+    sum += curr->value;
+    curr = curr->next;
+  }
+  return sum;
+}
+
+const void query(struct LinkedList* head, int index) {
+  int idx = 0;
+  struct LinkedList* curr = head;
+  while (curr != NULL)
+  {
+    if (idx == index) {
+      printf("Index %d has value: %d\n", index, curr->value);
+      return;
+    }
+    curr = curr->next;
+    idx++;
+  }
+  printf("Index %d is out of range.\n", index);
+}
 
 int main(void) {
   size_t len;
   struct LinkedList* head = build_linked_list(&len);
   
+  puts("\n");
+  
+  printf("Sum of linked list: %d \n", summation(head));
+
+  query(head, 0);
+  query(head, 3);
+  query(head, 2);
+
+  puts("\n");
+  puts("Destroying....\n");
   struct LinkedList* curr = head;
   while (curr != NULL)
   {
@@ -55,9 +92,9 @@ int main(void) {
   
     free(temp);
   }
-
   puts("\n");
-  
+
+  puts("Completed :)");
 
   return 0;
 }
