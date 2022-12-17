efa48501b53dec8d78068cf8f8b887ea64c6024f
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-16 19:48:38 -0800

    arithmetic lang processor done

--- arithmetic-parser/arithmetic_parser.c
+++ arithmetic-parser/arithmetic_parser.c
@@ -8,9 +8,7 @@
  * <raw> ::= <digit> | <digit> <raw>
  * <number> ::= <digit> | <not_zero> <raw>
  * <str> ::= <number> '0'
- * 
- * EXPR TODO:
- * <expr> ::= <number> | <number> '+' <expr> | <number> '-' <expr> 
+ * <expr> ::= <number> | <number> '+' <expr> | <number> '-' <expr> |  <number> '*' <expr> 
  * **/
 
 const char* stream = NULL;
@@ -23,16 +21,19 @@ bool accept(char c) {
   return false;
 }
 
+// <not_zero> ::= '1'|...|'9'
 bool notzero(void) {
   return accept('1') || accept('2') || accept('3') || accept('4') ||
          accept('5') || accept('6') || accept('7') || accept('8') || 
          accept('9');
 }
 
+// <digit> ::= '0' | <not_zero>
 bool digit(void) {
   return accept('0') || notzero();
 }
 
+// <raw> ::= <digit> | <digit> <raw>
 bool raw(void) {
   if (digit()) {
     raw();
@@ -41,6 +42,7 @@ bool raw(void) {
   return false;
 }
 
+// <number> ::= <digit> | <not_zero> <raw>
 bool number() {
   if (notzero()) {
     raw();
@@ -49,17 +51,34 @@ bool number() {
   return accept('0');
 }
 
+// <str> ::= <number> '0'
 bool str() {
   return number() && accept('\0');
 }
 
-bool recursive_descent_check(const char* expr) {
-  stream = expr;
-  return str();
+// <expr> ::= <number> | <number> '+' <expr> | <number> '-' <expr> 
+bool expr() {
+  bool c1 = number();
+  
+  if (c1 && accept('\0')) {
+    return true;
+  }
+
+  if (c1 && (accept('+') || accept('-') || accept('*'))){
+    return expr();
+  }
+
+  return false;
+};
+
+bool recursive_descent_check(const char* test_expr) {
+  stream = test_expr;
+  // return str();
+  return expr();
 };
 
 int main(void) {
-  const char* test_exprs[6] = { "304002", "1240", "123", "02342", "23c42", "abc"};
+  const char* test_exprs[6] = { "3040+2", "1-240", "125*4053", "02342", "23c42", "abc"};
   const bool expectations[6] = { true, true, true, false, false, false};
 
   for (int i = 0; i < 6; i++) {
